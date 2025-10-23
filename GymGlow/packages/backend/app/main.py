05cbf63d132a83ab1777bda74d58cf2f7c1b57a5
Keren Stoller <keren123stoller@gmail.com>
2025-10-23 08:52:43 +0300

    added frontend on render to allowed origin for backend

--- GymGlow/packages/backend/app/main.py
+++ GymGlow/packages/backend/app/main.py
@@ -12,7 +12,11 @@ app = FastAPI(title="GymGlow API",
 # Allow local frontend to use cookies (HttpOnly JWT) with cross-origin requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://frontend-react-csa1.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],)
